51691b801c4b1029bf1c353a8ab6b3fdf6fcb8d9
Tri Nguyen <tri.seddon.nguyen@gmail.com>
2018-11-01 01:56:20 -0600

    Created SPA

---
+++ challenge_2/spa/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+  <!-- the router outlet, where all matched components would ber viewed -->
+  <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+}
+</script>
+<!-- styling for the component -->
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ challenge_2/spa/src/components/Hello.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="Hello">
+    <label for="time">Time</label>
+    <input type="text" id="time" v-model="epochTime" readonly>
+    <button @click="getEpochTime">Click here</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            epochTime: ''
+        };
+    },
+    methods: {
+        getEpochTime() {
+          this.epochTime = new Date().getTime();
+        }
+    },
+}
+</script>
